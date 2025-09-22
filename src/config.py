16ebbd2084c394af011cde82c5c7f77aa7722ba5
RKirill28 <pavlov26.2008@gmail.com>
2025-09-22 20:43:00 +0700

    added database config

--- src/config.py
+++ src/config.py
@@ -5,8 +5,12 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class BotConfig(BaseSettings):
     token: str
 
+class DBConfig(BaseSettings):
+    url: str
+
 class Settings(BaseSettings):
     bot_config: BotConfig
+    db_config: DBConfig
 
     model_config = SettingsConfigDict(
         env_file=(
