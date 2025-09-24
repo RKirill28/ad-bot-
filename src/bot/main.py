f9fc8326de5c11756d777a516c937cd430bb561f
RKirill28 <pavlov26.2008@gmail.com>
2025-09-25 02:09:17 +0700

    change get main menu

--- src/bot/main.py
+++ src/bot/main.py
@@ -1,6 +1,7 @@
-from aiogram import Router
+from aiogram import F, Router
 from aiogram.filters import CommandStart
-from aiogram.types import Message
+from aiogram.types import CallbackQuery, Message
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from bot.keyboards import admin_menu, moder_menu, user_menu
 
@@ -12,22 +13,8 @@ moders = [7375913205]
 
 
 @router.message(CommandStart())
-async def start(mess: Message) -> None:
-    reply_markup = None
-    text = ""
-    if mess.from_user.id == admin:
-        reply_markup = admin_menu()
-        text = "Здравствуйте, господин. Ваше админ меню:"
-    elif mess.from_user.id in moders:
-        reply_markup = moder_menu()
-        text = "Здравствуйте, дорогой модератор! Это ваше меню, удачной модерации:"
-    else:
-        reply_markup = user_menu()
-        text = (
-            "Здравствуйте, это бот для размещения и просмотра рекламных объявлений.\n"
-        )
-        "Здесь вы можете легко и просто разместить рекламу о своем проекте, и также можете найти интересующий вас проект!\n\n"
-        "Это ваше меню, выберите один из пунктов:"
+async def start(mess: Message, session: AsyncSession) -> None:
+    text, reply_markup = await message_service.get_main_menu(session, mess.from_user.id)
 
     await mess.answer(text=text, reply_markup=reply_markup)
 
