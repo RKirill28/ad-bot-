a7c47a29bd4a91d2c69dba831b232a930e8f9185
RKirill28 <pavlov26.2008@gmail.com>
2025-09-25 01:51:08 +0700

    added new methods for moder repo: get_all(get with child) and get_ids(get only ids)

--- src/db/repositories/moderator_repo.py
+++ src/db/repositories/moderator_repo.py
@@ -1,9 +1,24 @@
+from typing import Sequence
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import selectinload
 from db.models import Moderator
 from db.schemas import ModeratorData
 
-from repositories.base import BaseRepository
+from db.repositories.base import BaseRepository
 
 
 class ModeratorRepo(BaseRepository[Moderator, ModeratorData]):
     # NOTE: Here will special methods for ModeratorRepo(for example add_ad_for_moderate)
     model = Moderator
+
+    @classmethod
+    async def get_all(cls, session: AsyncSession) -> Sequence[Moderator]:
+        query = select(cls.model).options(selectinload(Moderator.in_moderating))
+        res = await session.execute(query)
+        return res.scalars().all()
+
+    @classmethod
+    async def get_ids(cls, session: AsyncSession) -> Sequence[int]:
+        res = await session.execute(select(cls.model.id))
+        return res.scalars().all()
