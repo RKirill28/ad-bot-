b9081f9d71e8589bf0cadda3700a6d29b27a70f4
RKirill28 <pavlov26.2008@gmail.com>
2025-09-25 20:23:20 +0700

    added service for admin meni

---
+++ src/bot/services/admin_service.py
@@ -0,0 +1,23 @@
+from aiogram.types import ChatFullInfo
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from db.repositories.moderator_repo import ModeratorRepo
+from db.schemas import ModeratorData
+
+class AlredyExists(Exception):
+    """Ошибка указывающая на то, что объект в бд уже существует"""
+
+async def add_moder(session: AsyncSession, user_info: ChatFullInfo) -> ModeratorData:
+    user = ModeratorData(
+        telegram_id=user_info.id,
+        first_name=user_info.first_name,
+        last_name=user_info.last_name,
+        username=user_info.username,
+    )
+    ModeratorRepo.create(session, user)  # Добаляем модера в бд
+
+    await session.commit()
+    return user
+
+async def remove_moder(session: AsyncSession, user_id: int) -> None:
+    await ModeratorRepo.remove(session, user_id)
