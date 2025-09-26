433bae903b71fd780543a0480537e4441e2b1c67
RKirill28 <pavlov26.2008@gmail.com>
2025-09-26 19:42:39 +0700

    added delete moder func

--- src/bot/admin_router.py
+++ src/bot/admin_router.py
@@ -15,7 +15,6 @@ from bot.keyboards import back_to_menu_btn
 import bot.services.message_service as message_service
 
 router = Router()
-moders = []
 
 
 async def send_main_menu(mess: Message, session: AsyncSession) -> None:
@@ -44,6 +43,57 @@ async def add_moder(cb: CallbackQuery, state: FSMContext) -> None:
         "Отправьте ID пользователя Telegram, которого хотите добавить как модератора:",
         reply_markup=back_to_menu_btn(),
     )
+
+
+@router.callback_query(F.data == "delete_moder")
+async def delete_moder(
+    cb: CallbackQuery, state: FSMContext, session: AsyncSession
+) -> None:
+    await cb.answer()
+
+    data = await moderator_serivce.get_moderators_list(session)
+    text = message_service.get_moders_list_text(data)
+
+    await cb.message.edit_text(text, reply_markup=keyboards.back_to_menu_btn())
+    await cb.message.answer("Напишите ID модератора, которого хотите удалить:")
+
+    await state.set_state(AdminStates.delete_moder)
+
+
+@router.message(AdminStates.delete_moder)
+async def delete_moder(
+    mess: Message, bot: Bot, state: FSMContext, session: AsyncSession
+) -> None:
+    text = mess.text.strip()
+
+    if not text.isdigit():
+        await mess.answer(
+            "Это не число, попробуйте еще раз:", reply_markup=back_to_menu_btn()
+        )
+
+    try:
+        user_info = await bot.get_chat(int(text))
+        try:
+            await admin_service.delete_moder(session, int(text))
+        except IntegrityError:
+            await mess.answer(
+                "Такого модера не сущетсвует, попробуйте еще раз:",
+                reply_markup=back_to_menu_btn(),
+            )
+            return
+
+        await mess.answer(
+            f"Удалил модера @{user_info.username} | {user_info.first_name} | {user_info.last_name}"
+        )
+
+        await state.clear()  # WARN: Чистим состояние, полностью
+
+        await send_main_menu(mess, session)
+    except TelegramBadRequest:
+        await mess.answer(
+            "Не удалось найти такого пользователя, попробуйте еще раз:",
+            reply_markup=back_to_menu_btn(),
+        )
 
 
 @router.message(AdminStates.add_new_moder)
